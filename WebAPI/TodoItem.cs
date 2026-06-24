7a11212b6592fd2657d6208ce3e56b59aa8af228
Fabian Blazovich <fab.blazovich@gmail.com>
2026-06-24 15:09:12 +0200

    refactor: change class TodoItem

---
+++ WebAPI/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace WebAPI
+{
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; }
+    }
+}
