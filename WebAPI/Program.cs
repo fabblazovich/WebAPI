a0ba4c4c4c525aa67821c3d0838ebea56da6aa35
Fabian Blazovich <fab.blazovich@gmail.com>
2026-06-26 11:52:32 +0200

    refactor: add MaterialToDoService

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ITodoService, ToDoService>();
+builder.Services.AddSingleton<ITodoService, MaterialToDoService>();
 
 var app = builder.Build();
 
---
+++ WebAPI/Services/MaterialToDoService.cs
@@ -0,0 +1,34 @@
+﻿
+namespace WebAPI.Services
+{
+    public class MaterialToDoService : ITodoService
+    {
+        private readonly List<TodoItem> _todos = new List<TodoItem>()
+        {
+            new TodoItem { ID = 1, Title = "Laptop vorbereiten", IsDone = true },
+            new TodoItem { ID = 2, Title = "USB-Kabel einpacken", IsDone = false },
+            new TodoItem { ID = 3, Title = "Notizbuch mitnehmen", IsDone = false },
+            new TodoItem { ID = 4, Title = "Präsentation testen", IsDone = true }
+        };
+
+        public bool Create(TodoItem todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetAll()
+        {
+            return _todos;
+        }
+
+        public TodoItem? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
